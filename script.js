cfc220b2b0db188a360572213966d54548b7b3c2
Darko <darkogligorijevic4@gmail.com>
2023-08-03 09:43:30 +0200

    Add player choice function

--- script.js
+++ script.js
@@ -2,10 +2,26 @@ const MAX = 2;
 const MIN = 0;
 
 
+
+function getPlayerChoice() {
+  const userChoice = prompt("It's your turn to pick (Rock, Paper, Scissor)");
+  return userChoice[0].toUpperCase() + userChoice.slice(1).toLocaleLowerCase();
+}
+
+// console.log(getPlayerChoice());
+
+
+
 function getComputerChoice() {
   const arrayOfChoice = ['Rock', 'Paper', 'Scissors'];
   const randomNumber = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
   return arrayOfChoice[randomNumber];
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
+
+// function playRound(playerSelection, computerSelection) {
+//   // your code here!
+// }
+
+// console.log(playRound(getPlayerChoice,getComputerChoice))
